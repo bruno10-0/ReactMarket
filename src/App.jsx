69becfa9510676561198baa8e43c9672e4d94fa7
Bruno <brunobrodon75@gmail.com>
2024-02-01 04:01:23 -0300

    feat: :lipstick: Implemented significant improvements to LaNav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,11 @@ import { NavBar2 } from './components/navBar/navBar'
 import { SliderAuto } from './components/infiniteSliderAuto/sliderAuto'
 function App() {
 
-
   return (
     <>
-         
       <NavBar2 />
       <CazySlider />
       <SliderAuto />
-
     </>
   )
 }
--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,4 +1,4 @@
-import { CiSearch, CiInstagram  } from "react-icons/ci";
+import { CiSearch, CiInstagram, CiShoppingCart } from "react-icons/ci";
 import { FaWhatsapp, FaBars } from "react-icons/fa";
 import { useState, useEffect } from "react";
 
@@ -19,21 +19,38 @@ export const NavBar2 = () => {
     }, [prevScrollPos]);
 
     const navStyle = {
-        top: visible ? "0" : "-60px",
+        top: visible ? "0" : "-70px",
     };
     return (
         <div>
             <header className="headerNav" style={navStyle}>
                 <input type="checkbox" name="" id="chk1" />
-                <div className="logo">
-                    <svg
-                        width="40"
-                        height="40"
-                        viewBox="0 0 76 65"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="#ffffff" />
-                    </svg>
+                <div className="logo flex items-center gap-8">
+                    <div className="nav-logo">
+                        <svg
+                            width="40"
+                            height="40"
+                            viewBox="0 0 76 65"
+
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M37.5274 0L75.0548 65H0L37.5274 0Z" fill="#ffffff" />
+                        </svg>
+                    </div>
+                    <div className="flex gap-8 items-center">
+                        <h1>
+                            <span className="text-lg font-bold">React</span>Market
+                        </h1>
+                        <div className="relative  cursor-pointer">
+                            <CiShoppingCart className="text-5xl"/>
+                            <i className="w-3 h-3 absolute top-2 right-0 rounded-full bg-red-400"></i>
+                            <p className="absolute top-4 left-5  text-xs text-center">20</p>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div>
+
                 </div>
                 <div className="search-box">
                     <form action="">
@@ -47,8 +64,8 @@ export const NavBar2 = () => {
                     <li><a href="#">Blog</a></li>
                     <li><a href="#">Contact</a></li>
                     <li>
-                        <a href="#"><CiInstagram className="text-2xl"/></a>
-                        <a href="#"><FaWhatsapp className="text-2xl"/></a>
+                        <a href="#"><CiInstagram className="text-2xl" /></a>
+                        <a href="#"><FaWhatsapp className="text-2xl" /></a>
                     </li>
                 </ul>
                 <div className="menu">
